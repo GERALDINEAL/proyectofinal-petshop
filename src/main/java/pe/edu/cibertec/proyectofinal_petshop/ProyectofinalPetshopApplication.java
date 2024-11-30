b800955e05cf67287271e407086f68295c143af2
geraldine <gera.a.aurente@gmail.com>
2024-11-30 00:41:37 -0500

    first commit

---
+++ src/main/java/pe/edu/cibertec/proyectofinal_petshop/ProyectofinalPetshopApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.proyectofinal_petshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectofinalPetshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectofinalPetshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/proyectofinal_petshop/ProyectofinalPetshopApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.proyectofinal_petshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectofinalPetshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
